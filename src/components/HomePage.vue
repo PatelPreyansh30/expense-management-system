c554f2501f6eac5d363a2070f502497d5ddc4020
Rushikesh2013 <falakrushikesh83@gmail.com>
2022-08-05 11:47:36 +0530

    "initail comiit"

---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+<i class='bx bx-menu'></i>
+<h1>home page</h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'HomePage'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import routers from './routers.js'
 
-createApp(App).mount('#app')
+createApp(App).use(routers).mount('#app')

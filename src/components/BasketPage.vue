ab1f6f74e3217d55a497d5572e5781f9109c433f
Rushikesh2013 <falakrushikesh83@gmail.com>
2022-08-05 15:01:47 +0530

    Basket page added

--- src/components/BasketPage.vue
+++ src/components/BasketPage.vue
@@ -1,9 +1,27 @@
 <template>
-    <h1>Basket Page</h1>
+<div class="search">
+    <input type="text" placeholder="Search.." v-model="search">
+    <i @click="searchQuery"  class='bx bx-search'></i>
+</div>
+<div class="basket-container">
+    <div class="basket-item">Date 1</div>
+    <div class="basket-item">Date 2</div>
+    <div class="basket-item">Date 3</div>
+</div>
 </template>
 
 <script>
 export default {
-    name:'BasketPage'
+    name:'BasketPage',
+    data() {
+        return {
+            search:null,
+        }
+    },
+    methods:{
+        searchQuery() {
+           console.log(this.search);
+        }
+    }
 }
 </script>

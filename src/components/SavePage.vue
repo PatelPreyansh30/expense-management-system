c14f926522ada67274e680d2bdf6c37596f7b536
Patel Preyansh <patelpreyansh3011@gmail.com>
2022-08-06 01:51:57 +0530

    save fix

--- src/components/SavePage.vue
+++ src/components/SavePage.vue
@@ -1,70 +1,54 @@
 <template>
-  <Header />
-  <div>
-    <label>Enter your monthly salary</label> <br />
-    <br />
-    <input type="text" placeholder="Enter your salary" v-model="salary" />
-    <br />
-    <br />
-    <button @click="saveAdd" type="submit">Save</button>
-  </div>
     <Header />
-    <div class="salary">
-        <div class="salary-item">
-
-            <label>Enter your monthly salary</label> <br> <br>
-        </div>
-        <div class="salary-item">
-
-            <input type="text" placeholder="Enter your salary" v-model="salary"> <br> <br>
-        </div>
-        <div class="salary-item">
-            
-            <button @click="saveAdd" type="submit">Save</button>
-        </div>
+    <div>
+        <label>Enter your monthly salary</label> <br />
+        <br />
+        <input type="text" placeholder="Enter your salary" v-model="salary" />
+        <br />
+        <br />
+        <button @click="saveAdd" type="submit">Save</button>
     </div>
+    
 </template>
 <script>
 import Header from "./Header.vue";
 import axios from "axios";
 
 export default {
-  name: "SavePage",
-  data() {
-    return {
-      salary: null,
-    };
-  },
-  methods: {
-    async saveAdd() {
-      let res = await axios.post("http://172.20.10.13:8000/user/userdata/", {
-          "user":localStorage.getItem('login-userid'),
-          "salary":this.salary
-      },
-      {
-          headers:{
-              Authorization: `Token ${localStorage.getItem('login-token')}`
-          }
-      }
-      );
-      if(res.status==201){
-          alert("Salaray Added, Thank You!")
-          this.$router.push('/home')
-      }
-      else{
-          alert("Error in adding salary...")
-      }
+    name: "SavePage",
+    data() {
+        return {
+            salary: null,
+        };
     },
-  },
-  components: { Header },
-};
-</script>
     methods: {
-        saveAdd() {
-            console.log("saved");
-        }
+        async saveAdd() {
+            let res = await axios.post("http://172.20.10.13:8000/user/userdata/", {
+                "user": localStorage.getItem('login-userid'),
+                "salary": this.salary
+            },
+                {
+                    headers: {
+                        Authorization: `Token ${localStorage.getItem('login-token')}`
+                    }
+                }
+            );
+            if (res.status == 201) {
+                alert("Salaray Added, Thank You!")
+                this.$router.push('/home')
+            }
+            else {
+                alert("Error in adding salary...")
+            }
+        },
     },
-    components: { Header }
+    components: { Header },
+methods: {
+    saveAdd() {
+        console.log("saved");
+    }
+},
+components: { Header }
 }
 </script>
 
@@ -79,10 +63,10 @@ export default {
     flex-wrap: wrap;
     flex-direction: column;
 }
-.salary-item label{
+
+.salary-item label {
     border-left: 2px solid black;
     display: inline-block;
     padding-left: 5px;
 }
-
 </style>

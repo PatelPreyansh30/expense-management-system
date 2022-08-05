a5bef333e4cde986243804dc049a140c9b5b93c5
Rushikesh2013 <falakrushikesh83@gmail.com>
2022-08-05 12:46:23 +0530

    save page added

--- src/components/SavePage.vue
+++ src/components/SavePage.vue
@@ -1,8 +1,23 @@
 <template>
-    <h1>Save Page</h1>
+<div>
+<label>Enter your monthly salary</label> <br> <br>
+<input type="text" placeholder="Enter your salary" v-model="salary"> <br> <br>
+<button @click="saveAdd" type="submit">Save</button>
+</div>
+    
 </template>
 <script>
 export default {
-    name:'SavePage'
+    name:'SavePage',
+    data() {
+        return {
+            salary:null,
+        }
+    },
+    methods:{
+        saveAdd() {
+            console.log('saved');
+        }
+    }
 }
 </script>
